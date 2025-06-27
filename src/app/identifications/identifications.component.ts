58484b271019769e3b5c98c1dcb5a7c220771f3c
Martial59110 <floquet.martial@gmail.com>
2025-06-27 12:23:56 +0200

    refactor(identifications): update identifications component logic

--- src/app/identifications/identifications.component.ts
+++ src/app/identifications/identifications.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-identifications',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './identifications.component.html',
-  styleUrl: './identifications.component.scss'
+  styleUrls: ['./identifications.component.scss']
 })
-export class IdentificationsComponent {
+export class IdentificationsComponent implements OnInit {
+  guildBlocks: any[] = [];
 
+  ngOnInit() {
+    this.refreshIdentifications();
+  }
+
+  refreshIdentifications() {
+    // Données fictives pour l'exemple
+    this.guildBlocks = [
+      {
+        guildName: 'Serveur de test',
+        searchTerm: '',
+        currentPage: 1,
+        totalPages: 1,
+        pageSize: 10,
+        identifications: [
+          { userName: 'Jean Dupont', type: 'Carte', date: new Date() },
+          { userName: 'Alice Martin', type: 'Badge', date: new Date() }
+        ]
+      }
+    ];
+  }
+
+  onGuildSearch(guild: any) {
+    // Ici tu pourras filtrer les identifications selon guild.searchTerm
+  }
+
+  confirmDeleteIdentification(identification: any) {
+    // Logique de suppression à ajouter
+    alert('Suppression de ' + identification.userName);
+  }
+
+  prevGuildPage(guild: any) {}
+  nextGuildPage(guild: any) {}
+  loadGuildIdentifications(guild: any, page: number, pageSize: number, searchTerm: string) {}
+
+  acceptIdentification(identification: any) {
+    // Logique d'acceptation à implémenter
+    alert('Identification acceptée pour ' + identification.firstName + ' ' + identification.lastName);
+  }
+
+  refuseIdentification(identification: any) {
+    // Logique de refus à implémenter
+    alert('Identification refusée pour ' + identification.firstName + ' ' + identification.lastName);
+  }
 }
