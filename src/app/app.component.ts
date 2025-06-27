160011f08144ae6f553d61bb82b601e02303cfbe
Martial59110 <floquet.martial@gmail.com>
2025-06-27 12:23:55 +0200

    refactor(app): update app component logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'front-bot';
+  constructor(public router: Router) {}
+
+  get isLoginPage(): boolean {
+    return this.router.url.startsWith('/login') || this.router.url.startsWith('/auth-callback-page');
+  }
 }
