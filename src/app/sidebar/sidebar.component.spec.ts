4e83a53abeaf3fd302a7076d7ba2413278c99aa0
Martial59110 <floquet.martial@gmail.com>
2025-06-27 12:23:56 +0200

    test(sidebar): update sidebar component spec

--- src/app/sidebar/sidebar.component.spec.ts
+++ src/app/sidebar/sidebar.component.spec.ts
@@ -1,6 +1,7 @@
+// Jasmine imports are global
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SidebarComponent } from './sidebar.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
@@ -8,7 +9,10 @@ describe('SidebarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SidebarComponent]
+      imports: [
+        SidebarComponent,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
 
@@ -19,5 +23,25 @@ describe('SidebarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have isOpen property initialized to false', () => {
+    expect(component.isOpen).toBe(false);
+  });
+
+  it('should toggle sidebar when toggleSidebar is called', () => {
+    expect(component.isOpen).toBe(false);
+    
+    component.toggleSidebar();
+    expect(component.isOpen).toBe(true);
+    
+    component.toggleSidebar();
+    expect(component.isOpen).toBe(false);
+  });
+
+  it('should close sidebar when closeSidebar is called', () => {
+    component.isOpen = true;
+    component.closeSidebar();
+    expect(component.isOpen).toBe(false);
   });
 });
