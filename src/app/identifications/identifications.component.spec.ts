1088437636b26ddb61549e595e192ded6692c3a5
Martial59110 <floquet.martial@gmail.com>
2025-06-27 12:23:56 +0200

    test(identifications): update identifications component spec

--- src/app/identifications/identifications.component.spec.ts
+++ src/app/identifications/identifications.component.spec.ts
@@ -1,5 +1,5 @@
+// Jasmine imports are global
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { IdentificationsComponent } from './identifications.component';
 
 describe('IdentificationsComponent', () => {
